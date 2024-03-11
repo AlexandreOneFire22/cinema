75f9e695d4c341dc3ba8aec7050a1375dfc9977f
GAUTHIER Alexandre <alexgau70@gmail.com>
2024-03-11 16:10:24 +0100

    test

---
+++ partie/menu.php
@@ -0,0 +1,32 @@
+<header>
+    <nav class="navbar navbar-expand-md bg-black-perso border-bottom border-3 border-danger" data-bs-theme="dark">
+        <div class="container-fluid">
+            <div class="d-flex justify-content-start">
+                <a class="navbar-brand fs-5 text-danger" href="#">movie-party</a>
+            </div>
+            <form class="d-flex justify-content-end">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="#contenu">A propos de la formation</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#avis">Avis</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#contact">Contact</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="btn btn-primary" href="#tarifs" role="button">Acheter maintenant</a>
+                        </li>
+                    </ul>
+                </div>
+            </form>
+        </div>
+    </nav>
+</header>
