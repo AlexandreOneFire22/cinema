d6fc3548aa856e56d96cc53fff6f5ab650f0b650
GAUTHIER Alexandre <alexgau70@gmail.com>
2024-03-27 10:23:21 +0100

    Revert "Delete partie directory"
    This reverts commit 4480872c9066fbe6129a124485deacc1ed2ed333.

---
+++ partie/details.php
@@ -0,0 +1,105 @@
+<?php
+// Récupère le paramètre d'URL "prenom"
+// Tester la présence du paramètre
+
+$id = null;
+
+if(isset($_GET["id"])) {
+    $id = $_GET["id"];
+
+// Récupérer la liste des étudiants dans la table etudiant
+
+//1. connexion à la base de données db_intro
+    /**
+     * @var PDO $pdo
+     */
+
+    require "../config/db_config.php";
+
+//2. Prépareration de la requête
+
+    $requete = $pdo->prepare("SELECT * FROM film WHERE id = $id");
+
+//3. Exécution de la requête
+    $requete->execute();
+
+//4. Récupération des enregistrements
+//1. enregistrement = 1 tableau associatif
+    $details = $requete->fetchAll(PDO::FETCH_ASSOC);
+
+
+
+    $requete = $pdo->prepare("SELECT * FROM film");
+
+//3. Exécution de la requête
+    $requete->execute();
+
+//4. Récupération des enregistrements
+//1. enregistrement = 1 tableau associatif
+    $films = $requete->fetchAll(PDO::FETCH_ASSOC);
+
+    if ($id-1>count($films)){
+        header("Location: ../index.php");
+        exit();
+    }
+
+}else{
+    header("Location: ../index.php");
+    exit();
+}
+
+
+$temps=$details[0]["durée"];
+
+$minute=$temps%60;
+$heure=($temps-$minute)/60;
+
+$temps="$heure H $minute"
+
+
+?>
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport"
+        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link href="../assets/css/cyborg-bootstrap.min.css" rel="stylesheet">
+  <title>détails</title>
+</head>
+<body class="text-blanc">
+
+<?php include_once "menu.php" ?>
+
+<section class="container mt-3">
+
+    <div class="d-flex justify-content-start bg-light">
+        <div>
+            <img src="<?=$details[0]["image"]?>" width="300px" class="m-3">
+        </div>
+        <div class="d-flex flex-column mb-3 mx-5" style="height: 450px;">
+
+            <div> <h1 class="m-3"><?= $details[0]["titre"] ?></h1></div>
+
+            <div class="text-center m-3"> <?= $details[0]["résumé"] ?></div>
+
+            <div class="mt-auto p-2">
+                <div class="d-flex justify-content-evenly">
+
+                    <div>durée : <?= $temps ?></div>
+                    <div> pays : <?= $details[0]["pays"] ?></div>
+                    <div> date de sortie : <?= $details[0]["date_sortie"] ?></div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+
+
+<script src="../assets/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
